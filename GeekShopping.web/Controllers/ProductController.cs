028e8c1d7c90fd38256ee05fe564b9931f9f8b47
Bruno Seraguza <shagrathkampf@gmail.com>
2023-06-26 23:55:34 -0300

    Criando as Views delete/update/create e seus respectivos metodos que serão chamados no front com asp-action no (fazendo o btn bater no metodo) metodos de delete/update/create

--- GeekShopping.web/Controllers/ProductController.cs
+++ GeekShopping.web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using GeekShopping.web.Services.IServices;
+﻿using GeekShopping.web.Models;
+using GeekShopping.web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.web.Controllers
@@ -16,6 +17,67 @@ namespace GeekShopping.web.Controllers
         {
             var product = await  _productService.FindAllProducts();
             return View(product);
+        }
+        public async Task<IActionResult>ProductCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>ProductCreate(ProductModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(model);
+                if(response != null)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+               return View(model);
+        }
+        public async Task<IActionResult>ProductUpdate(int id)
+        {
+            var model =  await _productService.FindById(id);
+
+            if(model != null)
+            return View(model);
+
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult>ProductUpdate(ProductModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+                if(response != null)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+               return View(model);
+        } 
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var model =  await _productService.FindById(id);
+
+            if(model != null)
+            return View(model);
+
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult>DeleteProduct(ProductModel model)
+        {
+          
+            
+                var response = await _productService.DeleteProduct(model.Id);
+                if(response)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            
+               return View(model);
         }
     }
 }
